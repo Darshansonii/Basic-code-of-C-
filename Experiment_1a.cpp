20b414c6a99327debebe299582c3b662ed8e3ba8
Darshansonii <darsan779d.dd@gmail.com>
2025-07-19 17:42:04 +0530

    Add files via upload

---
+++ Experiment_1a.cpp
@@ -0,0 +1,17 @@
+//Name-Darshan Soni
+//PRN-24070123034
+//Exp1
+
+//Hello World code
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello, World!";
+    return 0;
+}
+
+// Output
+// Hello, World!
+   
